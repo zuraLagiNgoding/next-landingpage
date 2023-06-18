fd0c57c4aa816131b82903b9dc8fda36f04b4cc5
zuralagingoding <jibau19@gmail.com>
2023-06-18 14:44:04 +0700

    Initial commit

---
+++ components/SliderData.js
@@ -0,0 +1,15 @@
+export const SliderData = [
+    {
+      image: '/gambar1.jpg',
+    },
+    {
+      image: '/gambar2.jpg',
+    },
+    {
+      image: '/gambar3.jpg',
+    },
+    {
+      image: '/gambar4.jpg',
+    },
+  ];
+  
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import Navbar from '@/components/Navbar';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <>
+    <Navbar/>
+    <Component {...pageProps} />
+    </>
+  
+  );
 }
+
+export default App;
