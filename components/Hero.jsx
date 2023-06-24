4b7efab75e54d4e457e4c1d09b1f179f8c4cc479
zuralagingoding <jibau19@gmail.com>
2023-06-24 20:26:38 +0700

    initial commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Hero = ({message, heading,}) => {
+const Hero = ({message, heading}) => {
   return (
       <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
           <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' />
